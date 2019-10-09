1441aa7e1962ab22cb06db8878403242ac967c51
dtomkins00 <46679468+dtomkins00@users.noreply.github.com>
2019-10-09 22:40:57 +0100

    Update GameCountdown.js
    Slightly altered

--- GameCountdown.js
+++ GameCountdown.js
@@ -1,40 +1,32 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-p {
-  text-align: center;
-  font-family: Arial;
-  font-size: 60px;
-  margin-top: 0px;
-}
-</style>
-</head>
-<body>
-
-<p id="GameCountdown"></p>
-
-<script>
-var countDownDate = new Date("Oct 3, 2019 13:46:00").getTime();
-
-var x = setInterval(function() {
-
-  var now = new Date().getTime();
-    
-  var distance = countDownDate - now;
-    
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  document.getElementById("GameCountdown").innerHTML = minutes + "m " + seconds + "s ";
-    
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("GameCountdown").innerHTML = "Place a square";
-  }
-}, 1000);
-</script>
-
-</body>
-</html>
+<!DOCTYPE HTML> 
+<html> 
+<head> 
+<style> 
+p { 
+  text-align: center; 
+  font-size: 60px; 
+} 
+</style> 
+</head> 
+<body> 
+<p id="demo"></p> 
+<script> 
+var deadline = new Date("Jan 5, 2018 15:37:25").getTime(); 
+var x = setInterval(function() { 
+var now = new Date().getTime(); 
+var t = deadline - now; 
+var days = Math.floor(t / (1000 * 60 * 60 * 24)); 
+var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60)); 
+var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60)); 
+var seconds = Math.floor((t % (1000 * 60)) / 1000); 
+document.getElementById("demo").innerHTML = days + "d "  
++ hours + "h " + minutes + "m " + seconds + "s "; 
+    if (t < 0) { 
+        clearInterval(x); 
+        document.getElementById("demo").innerHTML = "EXPIRED"; 
+    } 
+}, 1000); 
+</script> 
+  
+</body> 
+</html> 
