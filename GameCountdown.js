4c0b021289a1abb3e30807d1cd1c50ff64fe4d3d
dtomkins00 <46679468+dtomkins00@users.noreply.github.com>
2019-10-09 23:47:12 +0100

    Update GameCountdown.js
    More timer alterations

--- GameCountdown.js
+++ GameCountdown.js
@@ -1,32 +1,49 @@
-<!DOCTYPE HTML> 
-<html> 
-<head> 
-<style> 
-p { 
-  text-align: center; 
-  font-size: 60px; 
-} 
-</style> 
-</head> 
-<body> 
-<p id="demo"></p> 
-<script> 
-var deadline = new Date("Jan 5, 2018 15:37:25").getTime(); 
-var x = setInterval(function() { 
-var now = new Date().getTime(); 
-var t = deadline - now; 
-var days = Math.floor(t / (1000 * 60 * 60 * 24)); 
-var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60)); 
-var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60)); 
-var seconds = Math.floor((t % (1000 * 60)) / 1000); 
-document.getElementById("demo").innerHTML = days + "d "  
-+ hours + "h " + minutes + "m " + seconds + "s "; 
-    if (t < 0) { 
-        clearInterval(x); 
-        document.getElementById("demo").innerHTML = "EXPIRED"; 
-    } 
-}, 1000); 
-</script> 
-  
-</body> 
-</html> 
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  return {
+    'total': t,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+// if there's a cookie with the name myClock, use that value as the deadline
+if (document.cookie && document.cookie.match('myClock')) {
+  // get deadline value from cookie
+  var deadline = document.cookie.match(/(^|;)myClock=([^;]+)/)[2];
+}
+
+// otherwise, set a deadline 10 minutes from now and 
+// save it in a cookie with that name
+else {
+  // create deadline 10 minutes from now
+  var timeInMinutes = 5;
+  var currentTime = Date.parse(new Date());
+  var deadline = new Date(currentTime + timeInMinutes * 60 * 1000);
+
+  // store deadline in cookie for future reference
+  document.cookie = 'myClock=' + deadline + '; path=/; domain=.placeholder.com';
+}
+initializeClock('clockdiv', deadline);
