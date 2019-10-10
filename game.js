1963c8f8d5fb296072b226c1827dce96b8bbfa2d
Husain Mehdi <husmehdi@gmail.com>
2019-10-10 14:28:30 +0100

    Added player state, better user tracking

--- game.js
+++ game.js
@@ -1,17 +1,25 @@
+//size of grid
 var numberOfRows = 20;
 var numberOfColumns = 30;
 
+//colour for next clicked cell
 var selectedColour = "blue";
+
+//state for all owned cells (rock/paper/scissors)
+var playerState = "scissors";
+
+//player's username - retrieved from database if existing user
+var playerName = prompt("Please enter your name");
+
 
 
 
 //2d array for grid data (make this track the owners)
-var gridData = new Array(numberOfColumns);
+var gridOwner = new Array(numberOfColumns);
 
 for (var i = 0; i < numberOfColumns; i++) {
-	gridData[i] = new Array(numberOfRows);
+	gridOwner[i] = new Array(numberOfRows);
 };
-
 
 //2d array for grid cells
 var gridCell = new Array(numberOfColumns);
@@ -20,12 +28,18 @@ for (var i = 0; i < numberOfColumns; i++) {
 	gridCell[i] = new Array(numberOfRows);
 };
 
-
 //2d array for cell colours
 var gridColour = new Array(numberOfColumns);
 
 for (var i = 0; i < numberOfColumns; i++) {
-	gridCell[i] = new Array(numberOfRows);
+	gridColour[i] = new Array(numberOfRows);
+};
+
+//2d array for cell states (rock/paper/scissors)
+var gridState = new Array(numberOfColumns);
+
+for (var i = 0; i < numberOfColumns; i++) {
+	gridState[i] = new Array(numberOfRows);
 };
 
 
@@ -64,41 +78,50 @@ function clickableGrid( rows, cols, callback ){
 //GAMEPLAY:
 
 //temp: on load, select random starting square
-gridData[Math.floor(Math.random() * 10)][Math.floor(Math.random() * 10)] = "mine";
+
+gridOwner[Math.floor(Math.random() * 10)][Math.floor(Math.random() * 10)] = playerName;
 
 
 //change colour of cell to selectedColour
 function changeColour(colour, col, row){
 	gridCell[col][row].style.background = colour;
-
+	gridColour[col][row] = colour;
 }
 
 
-
+//when cell is clicked, check adjacent cell then change
 var lastClicked;
 var grid = clickableGrid(numberOfRows,numberOfColumns,function(el,row,col,i){
-	//check adjacent cells (error when out of limits )
-	if(gridData[col][row+1]=="mine" || gridData[col][row-1]=="mine"){
-		gridData[col][row] = "mine";
-		changeColour(selectedColour, col, row);
-	}
-	else if(col == 0){
-		if(gridData[col+1][row]=="mine"){
-			gridData[col][row] = "mine";
-			changeColour(selectedColour, col, row);
-		}
-	}
-	else if(col == numberOfColumns-1){
-		if(gridData[col-1][row]=="mine"){
-			gridData[col][row] = "mine";
-			changeColour(selectedColour, col, row);
-		}
-	}
-	else if(gridData[col+1][row]=="mine" || gridData[col-1][row]=="mine"){
-		gridData[col][row] = "mine";
-		changeColour(selectedColour, col, row);
-	}
-
+	//check adjacent cells type
+    if((gridState[col][row]==undefined)||(gridState[col][row]=="scissors"&&playerState=="rock")||(gridState[col][row]=="paper"&&playerState=="scissors")||(gridState[col][row]=="rock"&&playerState=="paper")){
+        
+        //check adjacent cells ownership (error when out of limits )
+        if(gridOwner[col][row+1]==playerName || gridOwner[col][row-1]==playerName|| gridOwner[col][row]==playerName){
+            gridOwner[col][row] = playerName;
+            changeColour(selectedColour, col, row);
+            gridState[col][row] = playerState;
+        }
+        else if(col == 0){
+            if(gridOwner[col+1][row]==playerName){
+                gridOwner[col][row] = playerName;
+                changeColour(selectedColour, col, row);
+                gridState[col][row] = playerState;
+            }
+        }
+        else if(col == numberOfColumns-1){
+            if(gridOwner[col-1][row]==playerName){
+                gridOwner[col][row] = playerName;
+                changeColour(selectedColour, col, row);
+                gridState[col][row] = playerState;
+            }
+        }
+        else if(gridOwner[col+1][row]==playerName || gridOwner[col-1][row]==playerName){
+            gridOwner[col][row] = playerName;
+            changeColour(selectedColour, col, row);
+            gridState[col][row] = playerState;
+        }
+        
+    }
 
 	//debug: print coord of clicked 
     console.log(col, row);			
@@ -118,10 +141,21 @@ document.body.appendChild(grid);
 //updates grid periodically
 setInterval(function(){ for (var i = 0; i < numberOfColumns; i++) {
 							for (var j = 0; j < numberOfRows; j++) {
-								//debug: print owned
-								if(gridData[i][j]=="mine"){
-									console.log("owned:", i, j);
-									gridCell[i][j].innerHTML =  "mine";
+								//update owned cells
+								if(gridOwner[i][j]==playerName){
+                                    //debug: print owned
+                                    gridCell[i][j].innerHTML =  playerName;
+                                    
+                                    gridCell[i][j].style.borderColor = "gold";
+                                    gridCell[i][j].style.borderWidth = "5px 5px 5px 5px";
+                                    gridState[i][j] = playerState;
+                                }
+                                
+                                //update unowned cells
+								else if(gridOwner[i][j]!=null){
+									gridCell[i][j].innerHTML = gridOwner[i][j];
+                                    gridCell[i][j].style.background = gridColour[i][j];
+                                    gridCell[i][j].style.borderWidth = "0px 0px 0px 0px";
 								}
 							};
 						}; 
@@ -129,8 +163,85 @@ setInterval(function(){ for (var i = 0; i < numberOfColumns; i++) {
 
 
 
+//test multiplayer interaction
 
-//TO DO: Selector for colours, 2D array for grid colour, modify gridData to be gridOwner(owner of cells, compare own username with cells),
+function testPlace(col, row, colour, owner, state){
+	gridOwner[col][row] = owner;
+	gridColour[col][row] =  colour;
+	gridState[col][row] = state;
+}
+
+(function () {
+    console.log(Date.now());
+    testPlace(19,9,"red","P2","rock");
+    testPlace(20,9,"red","P2","rock");
+    testPlace(21,9,"red","P2","rock");
+    testPlace(22,9,"red","P2","rock");
+    testPlace(23,9,"red","P2","rock");
+    testPlace(24,9,"red","P2","rock");
+    testPlace(18,10,"red","P2","rock");
+    testPlace(19,10,"red","P2","rock");
+    testPlace(20,10,"red","P2","rock");
+    testPlace(21,10,"red","P2","rock");
+    testPlace(22,10,"red","P2","rock");
+    testPlace(23,10,"red","P2","rock");
+    testPlace(24,10,"red","P2","rock");
+    testPlace(25,10,"red","P2","rock");
+    testPlace(26,10,"red","P2","rock");
+    testPlace(27,10,"red","P2","rock");
+    testPlace(18,11,"brown","P2","rock");
+    testPlace(19,11,"brown","P2","rock");
+    testPlace(20,11,"brown","P2","rock");
+    testPlace(21,11,"NavajoWhite","P2","rock");
+    testPlace(22,11,"NavajoWhite","P2","rock");
+    testPlace(23,11,"NavajoWhite","P2","rock");
+    testPlace(24,11,"black","P2","rock");
+    testPlace(25,11,"NavajoWhite","P2","rock");
+    testPlace(17,12,"brown","P2","rock"); 
+    testPlace(18,12,"NavajoWhite","P2","rock");
+    testPlace(19,12,"brown","P2","rock");
+    testPlace(20,12,"NavajoWhite","P2","rock");
+    testPlace(21,12,"NavajoWhite","P2","rock");
+    testPlace(22,12,"NavajoWhite","P2","rock");
+    testPlace(23,12,"NavajoWhite","P2","rock");
+    testPlace(24,12,"black","P2","rock");
+    testPlace(25,12,"NavajoWhite","P2","rock");
+    testPlace(26,12,"NavajoWhite","P2","rock");
+    testPlace(27,12,"NavajoWhite","P2","rock");
+    testPlace(17,13,"brown","P2","rock"); 
+    testPlace(18,13,"NavajoWhite","P2","rock");
+    testPlace(19,13,"brown","P2","rock");
+    testPlace(20,13,"brown","P2","rock");
+    testPlace(21,13,"NavajoWhite","P2","rock");
+    testPlace(22,13,"NavajoWhite","P2","rock");
+    testPlace(23,13,"NavajoWhite","P2","rock");
+    testPlace(24,13,"NavajoWhite","P2","rock");
+    testPlace(25,13,"brown","P2","rock");
+    testPlace(26,13,"NavajoWhite","P2","rock");
+    testPlace(27,13,"NavajoWhite","P2","rock"); 
+    testPlace(28,13,"NavajoWhite","P2","rock");
+    testPlace(17,14,"brown","P2","rock");
+    testPlace(18,14,"brown","P2","rock");
+    testPlace(19,14,"NavajoWhite","P2","rock");
+    testPlace(20,14,"NavajoWhite","P2","rock");
+    testPlace(21,14,"NavajoWhite","P2","rock");
+    testPlace(22,14,"NavajoWhite","P2","rock");
+    testPlace(23,14,"NavajoWhite","P2","rock");
+    testPlace(24,14,"brown","P2","rock");
+    testPlace(25,14,"brown","P2","rock");
+    testPlace(26,14,"brown","P2","rock");
+    testPlace(27,14,"Brown","P2","rock");
+    testPlace(19,15,"NavajoWhite","P2","rock");
+    testPlace(20,15,"NavajoWhite","P2","rock");
+    testPlace(21,15,"NavajoWhite","P2","rock");
+    testPlace(22,15,"NavajoWhite","P2","rock");
+    testPlace(23,15,"NavajoWhite","P2","rock");
+    testPlace(24,15,"NavajoWhite","P2","rock");
+    testPlace(25,15,"NavajoWhite","P2","rock");
+    testPlace(26,15,"NavajoWhite","P2","rock");
+})();
+
+//TO DO: Selector for colours, 2D array for grid colour, modify gridOwner to be gridOwner(owner of cells, compare own username with cells),
 // check that selected grid ownership/ colour is different 
 
 
