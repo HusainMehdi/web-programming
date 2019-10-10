b06d94aaa38b352fbcb70540fc0145b15116711d
dtomkins00 <46679468+dtomkins00@users.noreply.github.com>
2019-10-10 12:41:06 +0100

    Added comments

--- GameCountdown.js
+++ GameCountdown.js
@@ -1,3 +1,4 @@
+//This function gets the current time remaining by getting the date and checking how many minutes and seconds are left in the countdown.
 function getTimeRemaining(endtime) {
   var t = Date.parse(endtime) - Date.parse(new Date());
   var seconds = Math.floor((t / 1000) % 60);
@@ -9,11 +10,15 @@ function getTimeRemaining(endtime) {
   };
 }
 
+//This function is what triggers the countdown by setting up the variables for time in the clock alongside the clock itself.
+//It also passes the endtime variable into the updateClock function.
 function initializeClock(id, endtime) {
   var clock = document.getElementById(id);
   var minutesSpan = clock.querySelector('.minutes');
   var secondsSpan = clock.querySelector('.seconds');
 
+  //The clock is updating itself using the getTimeRemaining function to place the values of the seconds and minutes variables
+  //into the HTML so it can continously update itself - once the total time reaches 0 the timeInterval variable is cleared.
   function updateClock() {
     var t = getTimeRemaining(endtime);
 
@@ -25,6 +30,7 @@ function initializeClock(id, endtime) {
     }
   }
 
+  //This part updates the clock every 1000 milliseconds, using the timeInterval variable to to this.
   updateClock();
   var timeinterval = setInterval(updateClock, 1000);
 }
@@ -46,4 +52,6 @@ function initializeClock(id, endtime) {
   // store deadline in cookie for future reference
  // document.cookie = 'myClock=' + deadline + '; path=/; domain=.placeholder.com';
 }
+
+//This part is what calls the initializeClock function on the site so that the timer will start counting down.
 initializeClock('clockdiv', deadline);
