bf2e18eca07835fcc30f59d79404ec5f7bd28e05
Callumtw42 <callumtw42@gmail.com>
2019-11-04 18:28:39 +0000

    Players can now see eachothers colors and state

--- clear.php
+++ clear.php
@@ -5,18 +5,16 @@ require('db.php');
 
 echo 'Processing.....';
 
-    $query = "UPDATE `gridcells` SET `hilighted` = FALSE";
+$query = "UPDATE `gridcells` SET `hilighted` = FALSE, `owner` = '', `colour` = 'white', `state` = '' ;";
 
-    if (mysqli_query($conn, $query)) {
-        echo 'db cleared...';
-    } else {
-        echo 'ERROR: ' . mysqli_error($conn);
-    }
+if (mysqli_query($conn, $query)) {
+    echo 'db cleared...';
+} else {
+    echo 'ERROR: ' . mysqli_error($conn);
+}
 
 
 //Check for Get variable
 if (isset($_GET['hilighted'])) {
     echo 'GET: db cleared' . $_GET['hilighted'];
 }
-
-
--- update.php
+++ update.php
@@ -6,21 +6,23 @@ require('db.php');
 echo 'Processing.....';
 
 //Check for POST variable
- if (isset($_POST['hilighted'])) {
+if (isset($_POST['hilighted'])) {
     // $hilighted = mysqli_real_escape_string($conn, $_POST['hilighted']);
-     $col = mysqli_real_escape_string($conn, $_POST['col']);
-     $row = mysqli_real_escape_string($conn, $_POST['row']);
+    $col = mysqli_real_escape_string($conn, $_POST['col']);
+    $row = mysqli_real_escape_string($conn, $_POST['row']);
+    $owner = mysqli_real_escape_string($conn, $_POST['owner']);
+    $colour = mysqli_real_escape_string($conn, $_POST['colour']);
+    $state = mysqli_real_escape_string($conn, $_POST['state']);
+    $hilighted = mysqli_real_escape_string($conn, $_POST['hilighted']);
 
-     $hilighted = mysqli_real_escape_string($conn, $_POST['hilighted']);
 
 
 
-
-    $query = "UPDATE `gridcells` SET `hilighted` = $hilighted WHERE `col` = $col AND `row` =$row;";
+    $query = "UPDATE `gridcells` SET `hilighted` = $hilighted, `owner`= '$owner', `colour`= '$colour', `state` = '$state' WHERE `col` = $col AND `row` =$row;";
 
     //send back the updated cell
     if (mysqli_query($conn, $query)) {
-        $updatedCell = array("col"=>$col, "row"=>$row);
+        $updatedCell = array("col" => $col, "row" => $row);
         echo json_encode($updatedCell);;
     } else {
         echo 'ERROR: ' . mysqli_error($conn);
@@ -31,5 +33,3 @@ echo 'Processing.....';
 if (isset($_GET['hilighted'])) {
     echo 'GET: update successful' . $_GET['hilighted'];
 }
-
-
