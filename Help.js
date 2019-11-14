f80716d1366deaa42b6037848cf3612d823f4b00
dtomkins00 <46679468+dtomkins00@users.noreply.github.com>
2019-11-14 11:15:56 +0000

    Update Help.js

--- Help.js
+++ Help.js
@@ -1,6 +1,20 @@
 function openWindow() {
     newWindow = window.open("", null, "height=200,width=400,status=yes,toolbar=no,menubar=no,location=no");  
   
-    newWindow.document.write("test");
+    newWindow.document.write("Hello, and welcome to Grid Conquest. The goal of this game is to conquer the grid of squares on screen.");
+    
+    newWindow.document.write("To do this, you need to click a square next to your initial starting square and then click another square next to either of your current squares - repeat this process as much as you need.");
+    
+    newWindow.document.write("However, your opponents will be looking to claim your squares as well. To stop them from taking your squares, you have a rock-paper-scissors type selector, which will change the type of the square you just placed. So if an opponent is using the rock type and you are using the paper type for example, they can't take your square. You would be able to take theirs due to your type advantage.")
+    
+    newWindow.document.write("You may also change your individual squares colour if you want to have some fun and make a picture.");
+    
+    newWindow.document.write("Once you have taken over all of the squares on the screen, you will have won the game! It will reset shortly afterwards however so you can play again.");
+    
+    newWindow.document.write("Have fun!");
+
+    ;
+
+
 
 }
