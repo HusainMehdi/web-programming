41cdf1f33b03dccb1fca9f4c2a670ebd4a76c6b7
dtomkins00 <46679468+dtomkins00@users.noreply.github.com>
2019-11-14 10:59:18 +0000

    Create Help.js

---
+++ Help.js
@@ -0,0 +1,6 @@
+function openWindow() {
+    newWindow = window.open("", null, "height=200,width=400,status=yes,toolbar=no,menubar=no,location=no");  
+  
+    newWindow.document.write("test");
+
+}
