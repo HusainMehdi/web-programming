26203bc04216f3b4437782e1e126ef1dbe86e568
dtomkins00 <46679468+dtomkins00@users.noreply.github.com>
2019-11-25 13:58:39 +0000

    Removed redundant JS

--- JS/help.js
+++
@@ -1,6 +0,0 @@
-var helpButton = document.querySelector("#helpButton");
-helpButton.addEventListener("click", help, false);
-
-function myFunction() {
-  window.open("C:\Users\Drew\Documents\GitHub\web-programming\helppage.html");
-}
